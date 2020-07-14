16320f73349e0f977243a4e9b6411ab9789a7c4c
Mory W <matt@skitskat.xyz>
2020-07-14 11:20:42 -0500

    Add hash() method that returns key hashed and compressed to fit array size

--- HashMap.js
+++ HashMap.js
@@ -3,6 +3,29 @@ class HashMap {
     this.hashmap = new Array(size)
       .fill(null);
   }
+
+  hash(key) {
+    let hashCode = 0
+    let hashCode2 = 0
+    for (let i = 0; i < key.length; i++) {
+      hashCode += key.charCodeAt(i)
+      hashCode2 += hashCode2 + key.charCodeAt(i)
+      // console.log(key.charCodeAt(i))
+      // console.log(`Hash code 1 = ${hashCode}`)
+      // console.log(`Hash code 2 = ${hashCode2}`)
+    }
+
+    hashCode = hashCode % this.hashmap.length
+    // console.log(`Hash code 1 = ${hashCode}`)
+    hashCode2 = hashCode2 % this.hashmap.length
+    // console.log(`Hash code 2 = ${hashCode2}`)
+
+    return hashCode
+  }
+
 }
+
+const hashMap = new HashMap(3)
+console.log(hashMap.hash('id'))
 
 module.exports = HashMap;
